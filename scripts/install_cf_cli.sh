9d906d5c60460c7b6d45f738119fa72610b4a63d
Brian Kendzior <bkendzior@gmail.com>
2018-06-12 15:28:14 -0700

    Create install_cf_cli.sh

---
+++ scripts/install_cf_cli.sh
@@ -0,0 +1,6 @@
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
+sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+sudo yum install cf-cli
