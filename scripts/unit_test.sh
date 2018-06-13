099bc53cb639fa5c04c7026a69f8263b8106bdeb
Brian Kendzior <bkendzior@gmail.com>
2018-06-13 11:31:58 -0700

    Update unit_test.sh

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -11,7 +11,7 @@ chmod +x ./scripts/install_cf_cli.sh
 ./scripts/install_cf_cli.sh
 
 # Configure CF ENV
-chmod +x ./scripts/configure_cf_cli.sh
+chmod +x ./scripts/configure_cf_cli.rb
 ./scripts/configure_cf_cli.rb
 
 # Running CF Tests
