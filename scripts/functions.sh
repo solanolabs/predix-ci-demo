6ef981a2d50a54f9c3d13a1473cf05b647e96be9
Brian Kendzior <bkendzior@gmail.com>
2018-06-20 14:50:57 -0700

    Update functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,6 +1,13 @@
 function install_docker {
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
   # Prepare to install docker
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update -y
   # Install docker and immediately stop it
